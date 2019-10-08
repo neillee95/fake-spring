c15ae24720fa6b7a6119217abc4312b667eff226
lee <crossthebackstreet@gmail.com>
2019-10-08 12:06:22 +0800

    添加class文件扫描

--- framework/src/main/java/me/lee/fakespring/framework/core/ClassScanner.java
+++ framework/src/main/java/me/lee/fakespring/framework/core/ClassScanner.java
@@ -2,18 +2,25 @@ package me.lee.fakespring.framework.core;
 
 import me.lee.fakespring.framework.util.StringUtil;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.JarURLConnection;
+import java.net.URISyntaxException;
 import java.net.URL;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.List;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
+import java.util.stream.Collectors;
 
 public class ClassScanner {
 
-    public static List<Class<?>> scanClasses(String packageName) throws IOException, ClassNotFoundException {
+    private static final String JAR_RESOURCE_PROTOCOL = "jar";
+    private static final String FILE_RESOURCE_PROTOCOL = "file";
+
+    public static List<Class<?>> scanClasses(String packageName) throws IOException, ClassNotFoundException, URISyntaxException {
         if (StringUtil.isEmpty(packageName)) {
             throw new NullPointerException("Package name is empty.");
         }
@@ -23,14 +30,14 @@ public class ClassScanner {
         Enumeration<URL> resources = classLoader.getResources(path);
         while (resources.hasMoreElements()) {
             URL resource = resources.nextElement();
-            if (resource.getProtocol().contains("jar")) {
+            String protocol = resource.getProtocol();
+            if (JAR_RESOURCE_PROTOCOL.equals(protocol)) {
                 JarURLConnection connection = (JarURLConnection) resource.openConnection();
                 String jarFilePath = connection.getJarFile().getName();
                 classes.addAll(getClassesFromJar(jarFilePath, path));
+            } else if (FILE_RESOURCE_PROTOCOL.equals(protocol)) {
+                classes.addAll(getClassesFromFiles(path, resource));
             }
-//            } else {
-//                //else
-//            }
         }
         return classes;
     }
@@ -49,6 +56,23 @@ public class ClassScanner {
             }
         }
         return classes;
+    }
+
+    private static List<Class<?>> getClassesFromFiles(String path, URL resource) throws URISyntaxException, IOException {
+        File file = new File(resource.toURI());
+        return Files.walk(file.toPath())
+                .filter(it -> it.toFile().getName().endsWith(".class"))
+                .map(it -> {
+                    String fileName = it.toString();
+                    try {
+                        return Class.forName(it.toString().replaceAll("/", ".")
+                                .substring(fileName.indexOf(path), fileName.indexOf(".class")));
+                    } catch (ClassNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                    return null;
+                })
+                .collect(Collectors.toList());
     }
 
 }
